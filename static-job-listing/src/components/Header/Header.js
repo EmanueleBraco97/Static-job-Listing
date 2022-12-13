adcf2a750faf4bb0a127f0190ada1e625a7b53b4
Emanuele Braco <emanuele.braco@gmail.com>
2022-12-13 16:24:25 +0100

    bugfix header

--- static-job-listing/src/components/Header/Header.js
+++ static-job-listing/src/components/Header/Header.js
@@ -1,7 +1,44 @@
+// import "./Header.css";
+
+// const Header = () => {
+//   return <header id="header"></header>;
+// };
+
+// export default Header;
+
+import { useContext } from "react";
+import { JobContext } from "../../context/JobContext";
+
 import "./Header.css";
 
 const Header = () => {
-  return <header id="header"></header>;
+  const { filterKeyword, RemoveKeyword, ClearKeywords } =
+    useContext(JobContext);
+  return (
+    <header className="header">
+      <div className={`${filterKeyword.length > 0 ? "search" : null}`}>
+        <div className="filter_tags">
+          {filterKeyword.map((tag, id) => {
+            return (
+              <div className="filter" key={id}>
+                <span>{tag}</span>
+                <img
+                  src="./images/icon-remove.svg"
+                  alt="remove-icon"
+                  onClick={() => RemoveKeyword(tag)}
+                />
+              </div>
+            );
+          })}
+        </div>
+        {filterKeyword.length > 0 && (
+          <button className="clear" onClick={ClearKeywords}>
+            Clear
+          </button>
+        )}
+      </div>
+    </header>
+  );
 };
 
 export default Header;
